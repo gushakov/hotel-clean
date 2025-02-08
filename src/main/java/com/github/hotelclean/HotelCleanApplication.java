7ff6a2b720863b64cd1c29578b7c7d4dcb26e8cb
gushakov <Georgy.Ushakov@unil.ch>
2025-02-08 08:46:36 +0100

    initial

---
+++ src/main/java/com/github/hotelclean/HotelCleanApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotelclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelCleanApplication.class, args);
+    }
+
+}
